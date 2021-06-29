99215428f3720fe669e922d333002d5f89543b38
DEEPANSHU GUPTA <deepanshu.gupta_cs18@gla.ac.in>
2021-06-30 00:28:44 +0530

    Add files via upload

---
+++ Py_1.py
@@ -0,0 +1,18 @@
+a = int(input("Enter first number")) 
+b = int(input("Enter first number"))
+sum = a+b
+sub = a-b
+mul = a*b
+div = a/b
+print("Sum of a and b is: ",sum )
+print("Difference of a and b is: ",sub )
+print("Product of a and b is: ",mul )
+print("Division of a and b is: ",div )
+
+#OUTPUT
+#Enter first number 10
+#Enter first number 2 
+#Sum of a and b is:  12      
+#Difference of a and b is:  8
+#Product of a and b is:  20  
+#Division of a and b is:  5.0
