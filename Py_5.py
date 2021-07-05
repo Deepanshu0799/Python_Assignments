8989ff2f08c3dcc7f6a84115a74ca7a9b8bf42d0
DEEPANSHU GUPTA <deepanshu.gupta_cs18@gla.ac.in>
2021-07-05 22:04:07 +0530

    Update Py_5.py
    PRIME OR NOT
    YOUTUBE DOWNLOADER

--- Py_5.py
+++ Py_5.py
@@ -17,3 +17,46 @@ else:
 #Yes, it is a prime number.
 #Enter a number: 8
 #Not a prime number.
+
+
+
+####################################################################################################################################################################################
+
+
+
+#Download from youtbe through python
+#pip install pytube
+
+from pytube import YouTube
+
+vid = YouTube(input("Enter the link of the video that you want to download : "))
+
+print(vid.title)
+
+print(vid.thumbnail_url)
+
+print(vid.length)
+
+print(vid.views)  
+
+print(vid.rating)
+
+streams = vid.streams
+
+for i in streams:
+    print(i)
+
+i = vid.streams.get_by_itag(input("Enter the itag number you see below for the preferred quality : "))
+
+
+file_size = i.filesize
+
+print(round(file_size/(1024*1024)), " MB")
+
+print("Download Started,wait till it gets downloaded...")
+
+i.download('C:/Users/Asus/Desktop/') 
+
+print("Download Completed ")
+
+
